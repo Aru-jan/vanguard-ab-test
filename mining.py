242e0228ecb5766924e060559ee02f46d7a3e7bf
jipijipi <jp.leflot@gmail.com>
2024-10-08 01:32:53 +0200

    started data import

---
+++ mining.py
@@ -0,0 +1,20 @@
+import yaml
+import pandas as pd
+
+# import data from txt file into pandas dataframe
+def import_data(path: str, separator: str = ',') -> pd.DataFrame:
+    data = pd.read_csv(path, sep=separator)
+    return data
+
+# import data from config.yaml
+def import_data_from_config(table_name: str) -> pd.DataFrame:
+
+    with open('config.yaml', 'r', encoding='utf-8') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+        
+    table = config['tables'][table_name]
+    path = table.get('path')
+    separator = table.get('separator', ',')
+    data = import_data(path, separator)
+    return data
+
