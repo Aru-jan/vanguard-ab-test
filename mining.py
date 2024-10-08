9d70f7f7eeacbad8441a63536c819a0725fdd0c5
jipijipi <jp.leflot@gmail.com>
2024-10-08 16:28:34 +0200

    working merge

--- mining.py
+++ mining.py
@@ -2,9 +2,11 @@ import yaml
 import pandas as pd
 
 # import data from txt file into pandas dataframe
-def import_data(path: str, separator: str = ',') -> pd.DataFrame:
-    #if list : merge tables
-    data = pd.read_csv(path, sep=separator)
+def import_data(paths: list, separator: str = ',') -> pd.DataFrame:
+    
+    print(paths)
+    data = pd.concat([pd.read_csv(path, sep=separator) for path in paths], axis=1)
+    #data = pd.read_csv(paths, sep=separator)
     return data
 
 # import data from config.yaml
@@ -12,9 +14,12 @@ def import_data_from_config(config, table_name: str) -> pd.DataFrame:
 
     table = config['tables'][table_name]
     print(table)
-    path = table.get('path')
+    paths = table.get('paths')
+    print(paths)
+    
     separator = table.get('separator', ',')
-    data = import_data(path, separator)
+    
+    data = import_data(paths, separator)
     return data
 
 def parse_config():
@@ -22,3 +27,6 @@ def parse_config():
         config = yaml.load(file, Loader=yaml.FullLoader)
         print(config)
     return config
+
+def merge_dfs(*dataframes):
+    return pd.concat(dataframes, axis=1)
