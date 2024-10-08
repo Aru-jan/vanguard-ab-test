182f5cae17edc6e840e3972a5853fbe3fc57c8a4
jipijipi <jp.leflot@gmail.com>
2024-10-08 22:25:39 +0200

    expanded df display function

--- cleaning.py
+++ cleaning.py
@@ -18,7 +18,37 @@ def select_columns(dataFrames, config):
         dataFrames[name] = dataFrame[selected_columns]
     return dataFrames
 
+def clean_categorical_data(dataFrames, config):
+    #TODO : default values for categories if no valid_categories
+    for table in config['tables']:
+        for column in config['tables'][table]['columns']:
+
+            column_config = config['tables'][table]['columns'][column]
+            valid_categories = column_config.get('valid_categories')
+
+            if valid_categories:
+                dataFrames[table][column] = dataFrames[table][column].astype('category')
+                dataFrames[table][column] = dataFrames[table][column].cat.set_categories(valid_categories)
+
+                fallback = column_config.get('fallback_category')
+                if fallback:
+                    dataFrames[table][column] = dataFrames[table][column].fillna(fallback)
+                else:
+                    dataFrames[table][column] = dataFrames[table][column].cat.add_categories(['unknown'])
+                    dataFrames[table][column] = dataFrames[table][column].fillna('unknown')
+    return dataFrames
+
 def display_categorical_value_counts(dataFrames):
+    #TODO : insert into display_dataFrames
     for dataFrame in dataFrames.values():
         for column in dataFrame.select_dtypes(include=['category']):
             display(dataFrame[column].value_counts())
+            
+def convert_types(dataFrames, config):
+    for table in config['tables']:
+        for column in config['tables'][table]['columns']:
+            column_config = config['tables'][table]['columns'][column]
+            if column_config['pandas_dtype'] == 'int64':
+                dataFrames[table][column] = dataFrames[table][column].fillna(0)  # or use dropna()
+            dataFrames[table][column] = dataFrames[table][column].astype(column_config['pandas_dtype'])
+    return dataFrames
--- mining.py
+++ mining.py
@@ -27,7 +27,30 @@ def parse_config():
         print(config)
     return config
 
-def display_dataFrames(dataFrames):
+def display_dataFrames(dataFrames, *args):
     for name, dataFrame in dataFrames.items():
-        display(dataFrame.dtypes)
-        display(name, dataFrame)
+        print(f'{name}:')
+        if 'frame' in args:
+            display(dataFrame)
+        if 'head' in args:
+            display(dataFrame.head())
+        if 'tail' in args:
+            display(dataFrame.tail())
+        if 'shape' in args:
+            display(dataFrame.shape)
+        if 'info' in args:
+            display(dataFrame.info())
+        if 'describe' in args:
+            display(dataFrame.describe())
+        if 'columns' in args:
+            display(dataFrame.columns)
+        if 'dtypes' in args:
+            display(dataFrame.dtypes)
+        if 'isnull' in args:
+            display(dataFrame.isnull().sum())
+        if 'value_counts' in args:
+            display(dataFrame.value_counts())
+        if 'unique' in args:
+            display(dataFrame.nunique())
+        if 'index' in args:
+            display(dataFrame.index)
