3efda83b930925bbd670dc154b030dd815ccb138
jipijipi <jp.leflot@gmail.com>
2024-10-08 17:07:39 +0200

    working selector

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,7 @@
-def rename_columns(df, column_names_dict):
-    df = df.rename(columns=column_names_dict)
-    return df
+def rename_columns(dataFrames, config):
+    for name, dataFrame in dataFrames.items():
+        new_names = {values['original_name']: new_name for new_name,
+                     values in config['tables'][name]['columns'].items()}
 
+        dataFrames[name] = dataFrame.rename(columns=new_names)
+    return dataFrames
