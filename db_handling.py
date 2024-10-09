4b90770802235413b2ce5beda1270ddfaa3aceb8
jipijipi <jp.leflot@gmail.com>
2024-10-09 18:00:12 +0200

    sql refacto, skippable db step

--- db_handling.py
+++ db_handling.py
@@ -21,28 +21,26 @@ def create_db(password, config):
 
 def export_dataframes_to_sql(engine, dataframes, config):
 
-    if config['refresh_db']:
-        for table_name, df in dataframes.items():
-            df.to_sql(name=table_name, con=engine,
-                      if_exists='replace', index=False)
-
-    else:
-        print('Skipping export, set refresh_db to true in config.yaml to export to SQL.')
+    for table_name, df in dataframes.items():
+        df.to_sql(name=table_name, con=engine,
+                  if_exists='replace', index=False)
 
 
 def import_data_from_sql(engine, table_name):
     data = pd.read_sql(f"SELECT * FROM {table_name}", engine)
     return data
 
+
 def import_all_tables_from_sql(engine):
     dataframes = {}
     inspector = inspect(engine)
     table_names = inspector.get_table_names()
-    
+
     for table_name in table_names:
         dataframes[table_name] = import_data_from_sql(engine, table_name)
-        
+
     return dataframes
+
 
 def export_dataframes_to_csv(dataframes):
     for table in dataframes:
